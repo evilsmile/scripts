b23ae8cd1961d7c33931196fa1eb4bc902cd9709
evilsmile <freedominmind@163.com>
2017-02-08 16:52:36 +0800

    use awk to do most match and filter work

--- Bash/confirm_mch_cardid.sh
+++ Bash/confirm_mch_cardid.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sql_cmd='mysql -u selectuser -pqrpos123 -h 192.168.1.174 --default-character-set=utf8'
+sql_cmd='mysql '
 
 db_info=""
 file=""
@@ -27,7 +27,7 @@ function check_in_log()
 
 	# 1. 从最新的更新日志中找出更新的时间
 	echo "-->>>>>> searching logs using database cardid ...."
-	dbcardid_update_log=$(grep --color=never -rnw "$db_card_id" /data/log/tfb_usermgr.log_* | grep -w UPDATE | grep -w F_card_id | tail -1)
+	dbcardid_update_log=$(grep --color=never -rnw "$db_card_id" /data/log/tfb_usermgr.log_* | grep -w UPDATE | grep -w card_id | tail -1)
 	dbcardid_update_date=$(echo $dbcardid_update_log | awk -F"[] []" '{print $2}')
 
 #	echo $dbcardid_update_log
@@ -35,7 +35,7 @@ function check_in_log()
 
 	# 2. 从最新的更新日志中找出更新的时间
 	echo "-->>>>>> searching logs using log cardid ...."
-	logcardid_update_log=$(grep --color=never -rnw "$cardid_from_log" /data/log/tfb_usermgr.log_* | grep -w UPDATE | grep -w F_card_id | tail -1)
+	logcardid_update_log=$(grep --color=never -rnw "$cardid_from_log" /data/log/tfb_usermgr.log_* | grep -w UPDATE | grep -w card_id | tail -1)
 	logcardid_update_date=$(echo $logcardid_update_log | awk -F"[] []" '{print $2}')
 
 #	echo $logcardid_update_log
@@ -54,12 +54,12 @@ function check_in_log()
 # 找出所有的成功D0过的商户
 # [mysql中也是用'#'作注释]
 # STEP 1. 
-sql="SELECT distinct F_mcht_id 
-		FROM trade.t_t0_flow 
-		WHERE F_state=31 
-				AND F_mcht_id like '%91' 
-				AND F_pay_channel_name!='d0_balance'
-#				AND F_mcht_id = '160000000010291'
+sql="SELECT distinct mcht_id 
+		FROM db.t_t0_flow 
+		WHERE state=31 
+				AND mcht_id like '%91' 
+				AND pay_channel_name!='d0_balance'
+#				AND mcht_id = '160000000023091'
 	"
 
 $sql_cmd -NBe "$sql" | while read mch_id ; do
@@ -69,17 +69,27 @@ $sql_cmd -NBe "$sql" | while read mch_id ; do
 
 	# STEP 2. 
 	# 找一个最新的订单号
-	info_from_db=$($sql_cmd -NBe "SELECT a.F_order_id,a.F_pay_channel_name, b.F_card_holder, b.F_card_id ,a.F_flow_id 
-				FROM trade.t_t0_flow a LEFT JOIN trade.t_mch_${mn} b ON a.F_mcht_id=b.F_mch_id
-				WHERE a.F_mcht_id='$mch_id' AND a.F_state=31 AND a.F_pay_channel_name!='d0_balance' 
-				ORDER BY a.F_create_time DESC LIMIT 1")
-
-	eval $( echo $info_from_db | awk '{ printf("                                 \
-							d0_order_prefix=%s;                                  \
-							d0_channel_name=%s;                                  \
-							card_holder=%s;                                      \
-							db_card_id=%s;										\
-							flow_id=%s",            substr($1, 0, 8), $2, $3, $4, $5)}')
+	eval $($sql_cmd -NBe "SELECT a.order_id,
+							a.pay_channel_name, 
+							b.card_holder,
+						    b.card_id, 
+							a.flow_id 
+				          FROM db.t_t0_flow a LEFT JOIN db.t_mch_${mn} b ON a.mcht_id=b.mch_id
+				          WHERE a.mcht_id='$mch_id' AND a.state=31 AND a.pay_channel_name!='d0_balance' 
+				          ORDER BY a.create_time DESC LIMIT 1" 			                             \
+							| awk '{ printf("                                                            \
+							d0_order_prefix=%s;                                  						 \
+							d0_channel_name=%s;                                  						 \
+							card_holder=%s;                                      						 \
+							db_card_id=%s;																 \
+							flow_id=%s",            													 \
+							substr($1, 0, 8),       													 \
+							$2,                     													 \
+							$3,                     													 \
+						    $4,                     													 \
+						    $5)                     													 \
+							}'                       													 \
+			)
 
 	db_info="$mch_id $card_holder $db_card_id"
 
@@ -87,48 +97,57 @@ $sql_cmd -NBe "$sql" | while read mch_id ; do
 	# 找到当天的D0日志文件
 	d0_log_file=/data/log/t0_service.log_${d0_order_prefix}
 
-	# STEP 4. 
-	key_log_line=$(grep -Hnw "INSERT.*($flow_id" $d0_log_file | head -1)
-
-	# 取出匹配到D0订单号的文件名和行号
-	eval $(echo $key_log_line | awk -F":" '{printf("file=%s;line=%s", $1, $2)}')
-
-	# 取出线程号, 作为后面的匹配
-	eval $(echo $key_log_line | awk -F"[][]" '{printf("thread_id=%s;", $10)}')
-
 	# STEP 5. 
-	# 从匹配到的行开始往后找50行该线程的日志, 对于一个请求50行日志足够了
-	thread_log=$(awk -v line=$line '{if(NR>=line) { print NR,$0 }}' $d0_log_file 	\
-						| grep -w $thread_id 										\
-						| head -50													\
-				)
-
-	# STEP 6. 
-	# 根据打款通道用不同的过滤
-	if [ $d0_channel_name == "daifu_tfb" ]; then
-		# 用acct_id 过滤出打款请求行，并过滤出acct_id=..acct_name=...
-		cardid_from_log=$(echo "$thread_log" 											 \
-						| grep -w "acct_id"    											 \
-						| head -1            											 \
-						| sed 's/.*acct_id=\(.*\)&acct_name=\(.*\)&business_type.*/\1/g' \
-						)
-	else
-		# 用cmbc_rt 找出打款请求行，并找到此行后面的<ACC_NO>..</ACC_NO>
-		first_xmcmbc_line=$(echo "$thread_log" 			\
-							| grep -w "cmbc_rt" -m 1    \
-							| awk '{print $1}'          \
-							)
-
-		if [ "X$first_xmcmbc_line" = "X" ];then
-			debug "$mch_id $NOTFOUND"
-			continue
-		fi
-
-		cardid_from_log=$(awk -v line=$first_xmcmbc_line '{if(NR>=line) {print}}' $d0_log_file   \
-						| grep -w ACC_NO -m 1 													 \
-						| sed 's/<ACC_NO>\([0-9]\{2,\}\)<\/AC.*/\1/g' 							 \
-				)
-	fi
+	# 从匹配到的行开始往后找该线程的日志
+	eval $(awk -v channel_name=$d0_channel_name  \
+					   'BEGIN{
+							card_id="-1"
+							xmcmbc_find=0
+							start_line=0
+							thread_id=0
+						}
+						{													 
+                            # 还未找到关键行
+							if (start_line == 0) {
+								if ( $0 ~ /INSERT.*\('$flow_id'/ ) {
+									start_line=NR
+									if (match($0, /^\[.*\]\[.*\]\[.*]\[.*\]\[(.*)\]-- .*/, arr) == 0) {
+										cardid_from_log=0
+                                        exit
+									}
+                                    # 找到关键行，取出线程id
+									thread_id=arr[1]
+								} 
+							} else {
+                                # 从关键行之后开始，根据不同的通道匹配不同的关键字
+								if(NR>=start_line) {
+						
+									if ($0 ~ thread_id && channel_name == "daifu_tfb") {
+                                        # 匹配acc_id=...模式，取出卡号
+										if (match($0, /.*acct_id=(.*)\&acct_name.*/, arr) > 0) {
+											card_id=arr[1]
+										}
+									} else {
+                                        # 加上新的匹配字符串确认发起请求的行
+										pat=thread_id".*cmbc_rt"
+			 							if ($0 ~ pat) {
+											xmcmbc_find=1
+										}
+                                        # 在后续的行找到ACC_NO模式，取出卡号
+										if (xmcmbc_find == 1 && match($0, /.*<ACC_NO>(.*)<\/AC/, arr) > 0) {
+											card_id=arr[1]
+										}
+									}
+								}
+								if (length(card_id)>5) {
+									exit
+								}
+							}
+					   }
+					   END{
+						printf("cardid_from_log=%s", card_id)
+					   }' $d0_log_file  					  \
+			 )
 
 	if [ "X$cardid_from_log" == "X" ]; then
 		debug "$mch_id $NOTFOUND"
