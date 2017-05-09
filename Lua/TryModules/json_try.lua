36c22815dafdaa58f5b8f1fa7bd5741ac4db9ffa
freedominmind <freedominmind@163.com>
2017-05-09 10:42:26 +0800

    add json decode/encode test for array and object

--- Lua/TryModules/json_try.lua
+++ Lua/TryModules/json_try.lua
@@ -5,13 +5,44 @@
 
 local json = require("json")
 
-local json_str = '{"name":"Anna", "age":30, "level":"top"}'
+-- json解析
+local json_str = [[
+        {
+            "name":"Anna",
+            "age":30,
+            "level":"top",
+            "places":["hill", "river"],
+            "info":{
+                "born":"ShiCheng",
+                "college":"GanzhouLigong"
+            }
+        }
+        ]]
 
 local json_tab = json.decode(json_str)
 for k, v in pairs(json_tab) do
-    print(k, v)
+    if (type(v) == "table") then
+        io.write(string.format("%s:", k ))
+        for k2, v2 in pairs(v) do
+           io.write(k2 .. " : " .. v2 .. ' ')
+        end
+        io.write('\n')
+    else
+         print(k, v)
+    end
 end
 
-local tab = { "lover", 123, name = "XiaoXian", age = 30, level = "sub-top"}
+-- json编码
+
+local tab = { 
+       name = "XiaoXian", 
+       info = {        -- 对象
+            born = "FuJian", 
+            college="Nvzi"
+        },
+        sign = {123, 34},   -- 数组
+        age = 30,
+        level = "sub-top"
+    }
 
 io.write(json.encode(tab) .. "\n")
