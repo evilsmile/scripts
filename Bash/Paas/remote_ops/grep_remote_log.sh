a79a54e231631ac8223d56d81a5f6de34bb53b92
evilsmile <freedominmind@163.com>
2017-09-05 20:42:59 +0800

    add some options to control grep_remote_log.sh behavior

--- Bash/Paas/remote_ops/grep_remote_log.sh
+++ Bash/Paas/remote_ops/grep_remote_log.sh
@@ -7,20 +7,44 @@ TMPDIR=tmp/
 # 合并最后一列，把","换成"\n"然后唯一化，再合并起来，逗号连接
 SUPPORTED_COMP=$(sed -e 's/.* \([^ ]*\)/\1/g' -e 's/,/\n/g' $CONFIG_FILE | sort | uniq | awk  'BEGIN{ORS=","}{print}')
 
-if [ $# -lt 2 ]; then
-	echo "Usage: keyword component_name[$SUPPORTED_COMP]"
-exit
-fi
+function usage()
+{
+	echo "Usage: -m -k keyword -c component_name"
+	echo "       -m 找到一个就返回"
+	echo "       -k 指定关键字"
+	echo "       -c 指定组件名字 [$SUPPORTED_COMP]"
+	exit -1
+}
 
-keyword=$1
-component=$2
 today=$(date +"%Y%m%d")
+
+keyword=""
+component=""
+grep_options="-rnw"
+
+while getopts "mk:c:" arg; do 
+    case $arg in
+      m)
+		grep_options="-m 1 $grep_options"
+        ;;
+      k)
+        keyword=$OPTARG
+        ;;
+      c)
+        component=$OPTARG
+        ;;
+     esac
+done
+
+if [ -z "$keyword"  -o   -z "$component" ]; then
+	usage
+fi
 
 local_ip=$(ifconfig eth1 | grep "inet addr" | sed 's/.*inet addr:\([0-9\.]\+\).*/\1/g')
 
 log_file=/opt/paas/smsp5.0/smsp_${component}/logs/smsp_${component}_${today}.log
 
-remote_cmd="grep -rnw \"$keyword\" $log_file"
+REMOTE_CMD="grep $grep_options \"$keyword\" $log_file"
 
 # 由组件名搜索对应的IP
 chosen_ips=$(grep --color=never -rw $component $CONFIG_FILE) 
@@ -48,9 +72,9 @@ while read host_ip user passwd components; do
 
 	if [ "$host_ip" == "$local_ip" ]; then
 		# 本机的话本地执行就可以
-		$remote_cmd >  $grep_result_file &
+		$REMOTE_CMD >  $grep_result_file &
     else
-		$ATOMOP_DIR/exe_remote.exp $host_ip $user $passwd "$remote_cmd" >  $grep_result_file &
+		$ATOMOP_DIR/exe_remote.exp $host_ip $user $passwd "$REMOTE_CMD" >  $grep_result_file &
 	fi
 done  < $TMPDIR/chose_ips.list
 
