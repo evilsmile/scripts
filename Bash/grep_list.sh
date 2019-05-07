a85fd52c36f58d2430245633cc7afcdd96ca5bd3
evilsmile <freedominmind@163.com>
2019-05-07 15:09:16 +0800

    add color remove of grep

--- Bash/grep_list.sh
+++ Bash/grep_list.sh
@@ -4,6 +4,10 @@ params="$*"
 
 result=$(grep $params)
 
+# 去掉色彩二进制
+# 先把二进制转换成文本, 替换, 再转换成二进制
+nocol_result=$(echo -n "$result" | hexdump -ve '1/1 "%.2x"' | sed -e 's/1b5b6d//g' -e 's/1b5b4b//g' -e 's/1b5b33..6d//g' -e 's/1b5b30313b33316d//g' | xxd -r -p)
+
 while true; do
     echo "$result" | awk 'BEGIN{i=1;}{printf("[%d]: %s\n", i, $0); i++}'
     read choice
@@ -11,7 +15,7 @@ while true; do
         break
     fi
 
-    eval $(echo "$result" | awk -F":" '{if(NR=='$choice'){printf("fn=%s;line=%s", $1, $2)}}')
+    eval $(echo "$nocol_result" | awk -F":" '{if(NR=='$choice'){printf("fn=%s;line=%s", $1, $2)}}')
     echo "file: $fn line: $line"
     vim $fn +$line
 done
